c538a6ff9181521a165cf1e930e6755df64e44fb
Indu Bakshi <bakshi.webworld@gmail.com>
2022-02-28 19:13:16 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use("/css", express.static(__dirname + "/public/css"));
+// app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/images", express.static(__dirname + "/public/images"));
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+app.get("/candidate-profile", function (req, res) {
+  res.render("pages/index");
+});
+var server = app.listen(5300, function () {
+  var port = server.address().port;
+  console.log("Server started at http://localhost:%s", port);
+});
