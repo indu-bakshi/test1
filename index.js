368824f2002b731b209436b0b1b3c377ec3357b4
Indu Bakshi <bakshi.webworld@gmail.com>
2022-03-03 12:23:13 +0530

    Test code

--- index.js
+++ index.js
@@ -2,32 +2,29 @@ var express = require("express");
 var favicon = require("serve-favicon");
 
 var app = express();
-app.use(favicon(__dirname + "/public/images/favicon.ico"));
+// app.use(favicon(__dirname + "/public/images/favicon.ico"));
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+// app.use(express.static("public"));
+// app.set("view engine", "ejs");
 
-app.use("/css", express.static(__dirname + "/public/css"));
-// app.use("/js", express.static(__dirname + "/public/js"));
-app.use("/images", express.static(__dirname + "/public/images"));
+// app.use("/css", express.static(__dirname + "/public/css"));
+// // app.use("/js", express.static(__dirname + "/public/js"));
+// app.use("/images", express.static(__dirname + "/public/images"));
 
 const port = process.env.PORT || 5300;
 
 app.get("/", function (req, res) {
   res.send("use /candidate-profile");
 });
-app.get("/candidate-profile", function (req, res) {
-  var userData = undefined;
-  if (req.query.userData) {
-    userData = req.query.userData;
-  } else {
-    userData = 1;
-  }
-  res.render("pages/index", {
-    userData: userData,
-  });
-});
-var server = app.listen(port, function () {
-  var port = server.address().port;
-  console.log("Server started at http://localhost:%s", port);
-});
+// app.get("/candidate-profile", function (req, res) {
+//   var userData = undefined;
+//   if (req.query.userData) {
+//     userData = req.query.userData;
+//   } else {
+//     userData = 1;
+//   }
+//   res.render("pages/index", {
+//     userData: userData,
+//   });
+// });
+app.listen(port, () => console.log(`Listening on port ${port}`));
