43bad9b51978c5c5ead0e6128c27252e779b95a0
Indu Bakshi <bakshi.webworld@gmail.com>
2022-04-08 10:24:34 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var favicon = require("serve-favicon");
+var fs = require("fs");
+var path = require("path");
 
 var app = express();
 app.use(favicon(__dirname + "/public/images/favicon.ico"));
@@ -8,33 +10,51 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.use("/css", express.static(__dirname + "/public/css"));
-// app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/js", express.static(__dirname + "/public/js"));
 app.use("/images", express.static(__dirname + "/public/images"));
 
 const port = process.env.PORT || 5300;
 
 app.get("/", function (req, res) {
-  var userData = undefined;
-  var company = undefined;
-  var tempid = undefined;
-  var cohort = undefined;
-
-  if (req.query.userData) {
-    userData = req.query.userData;
-    company = req.query.company;
-    tempid = req.query.tempid;
-    cohort = req.query.cohort;
+  var fileName = undefined;
+  if (req.query.profileBranch) {
+    fileName = path.join(
+      __dirname,
+      `/public/jsonData/${req.query.profileBranch}.json`
+    );
+    if (!fs.existsSync(fileName)) {
+      fileName = path.join(__dirname, "/public/jsonData/itComp.json");
+    }
   } else {
-    userData = 1;
-    company = 1;
-    tempid = -1;
-    cohort = -1;
+    fileName = path.join(__dirname, "/public/jsonData/itComp.json");
   }
-  res.render("pages/index", {
-    userData: userData,
-    company: company,
-    tempid: tempid,
-    cohort: cohort,
+  console.log(req.query.profileBranch);
+  fs.readFile(fileName, (err, data) => {
+    var jsonData = data;
+    var jsonParsed = JSON.parse(jsonData);
+    var userData = undefined;
+    var company = undefined;
+    var tempid = undefined;
+    var cohort = undefined;
+
+    if (req.query.userData) {
+      userData = req.query.userData;
+      company = req.query.company;
+      tempid = req.query.tempid;
+      cohort = req.query.cohort;
+    } else {
+      userData = 1;
+      company = 1;
+      tempid = -1;
+      cohort = -1;
+    }
+    res.render("pages/index", {
+      userData: userData,
+      company: company,
+      tempid: tempid,
+      cohort: cohort,
+      profileData: jsonParsed,
+    });
   });
 });
 app.get("/candidate-profile", function (req, res) {
